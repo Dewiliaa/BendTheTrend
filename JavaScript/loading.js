6f45daf82b302ebff0de6339501771ec0260ef45
Dewi Lia Virnada <S10262440@connect.np.edu.sg>
2024-02-08 20:21:09 +0800

    updated comments for loading

--- JavaScript/loading.js
+++ JavaScript/loading.js
@@ -1,5 +1,6 @@
 console.log('Need to wait');
 
+// Loads for 10 secs before going to summary.
 setTimeout(function() {
   console.log('setting window location');
   window.location.href = "/HTML/Summary.html";
